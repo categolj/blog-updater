c6a23bc5705c9403845d04044d9df9f1dcc2431b
making <makingx@gmail.com>
2017-04-14 00:26:03 +0900

    Polish

--- src/main/java/am/ik/blog/BlogUpdaterApplication.java
+++ src/main/java/am/ik/blog/BlogUpdaterApplication.java
@@ -3,14 +3,12 @@ package am.ik.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import am.ik.blog.entry.EntryUpdater;
 
 @SpringBootApplication
-@EnableWebFlux
 public class BlogUpdaterApplication {
 
 	public static void main(String[] args) {
