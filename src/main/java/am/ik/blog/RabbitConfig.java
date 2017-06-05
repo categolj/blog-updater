4479d4db913cc29e1e71a19bf530bbd6e2b2aa2b
making <makingx@gmail.com>
2017-06-06 01:21:56 +0900

    Rename and remove dataSource

--- src/main/java/am/ik/blog/RabbitConfig.java
+++ src/main/java/am/ik/blog/RabbitConfig.java
@@ -1,27 +1,16 @@
 package am.ik.blog;
 
-import javax.sql.DataSource;
-
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("cloud")
-public class CloudConfig extends AbstractCloudConfig {
+public class RabbitConfig extends AbstractCloudConfig {
 	@Bean
-	@Primary
 	ConnectionFactory rabbitConnectionFactory() {
 		return connectionFactory().rabbitConnectionFactory();
-	}
-
-	@Bean
-	@ConfigurationProperties(prefix = "spring.datasource.tomcat")
-	DataSource dataSource() {
-		return connectionFactory().dataSource();
 	}
 }
