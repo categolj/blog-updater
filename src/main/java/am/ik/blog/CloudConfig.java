8c4cf5b59aeaa32e7c775b30999b1ca546b24d2f
making <makingx@gmail.com>
2017-06-06 19:31:18 +0900

    Change dataSource configuration

--- src/main/java/am/ik/blog/CloudConfig.java
+++ src/main/java/am/ik/blog/CloudConfig.java
@@ -3,25 +3,64 @@ package am.ik.blog;
 import javax.sql.DataSource;
 
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
 
 @Configuration
 @Profile("cloud")
 public class CloudConfig extends AbstractCloudConfig {
+
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource.tomcat")
-	DataSource dataSource() {
-		return connectionFactory().dataSource(new DataSourceConfig(null,
-				new DataSourceConfig.ConnectionConfig("allowMultiQueries=true")));
+	DataSource dataSource(BlogDbCredentials credentials) {
+		return new DataSourceBuilder(ClassUtils.getDefaultClassLoader()) //
+				.url(credentials.getJdbcUrl() + "&allowMultiQueries=true") //
+				.username(credentials.getUsername()) //
+				.password(credentials.getPassword()) //
+				.driverClassName("org.mariadb.jdbc.Driver") //
+				.build();
 	}
 
 	@Bean
 	ConnectionFactory rabbitConnectionFactory() {
 		return connectionFactory().rabbitConnectionFactory();
+	}
+
+	@Component
+	@ConfigurationProperties(prefix = "vcap.services.blog-db.credentials")
+	public static class BlogDbCredentials {
+		private String jdbcUrl;
+		private String username;
+		private String password;
+
+		public String getJdbcUrl() {
+			return jdbcUrl;
+		}
+
+		public void setJdbcUrl(String jdbcUrl) {
+			this.jdbcUrl = jdbcUrl;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
 	}
 }
